999276c1d275f4f6b8d76f87fa784b33bd0a73af
mbed92 <michalbednarek.e@gmail.com>
2021-07-14 18:19:01 +0200

    ready to go LSTM learning

--- scripts/playground.py
+++ scripts/playground.py
@@ -3,51 +3,59 @@ import pybullet_data as pd
 
 p.connect(p.GUI)
 p.resetSimulation(p.RESET_USE_DEFORMABLE_WORLD)
-p.setAdditionalSearchPath(pd.getDataPath())
-restitutionId = p.addUserDebugParameter("restitution", 0, 1, 0.5)
-
-localInertiaDiagonalXId = p.addUserDebugParameter("localInertiaDiagonalX", 0, 3, 0.2)
-localInertiaDiagonalYId = p.addUserDebugParameter("localInertiaDiagonalY", 0, 3, 0.2)
-localInertiaDiagonalZId = p.addUserDebugParameter("localInertiaDiagonalZ", 0, 3, 0.2)
-
-lateralFrictionId = p.addUserDebugParameter("lateral friction", 0, 1, 0.5)
-spinningFrictionId = p.addUserDebugParameter("spinning friction", 0, 1, 0.03)
-rollingFrictionId = p.addUserDebugParameter("rolling friction", 0, 1, 0.03)
-
-plane = p.loadURDF("plane.urdf", [0, 0, 0])
-aaa = p.loadURDF("cube.urdf", [0, 0, 3], flags=p.URDF_USE_SELF_COLLISION)
-p.changeDynamics(aaa, -1, mass=0.1)
-
-
-def _load_softbody(basePos):
-    return p.loadSoftBody("cube.obj", basePosition=basePos, scale=0.5, mass=0.1, useNeoHookean=0,
-                          useBendingSprings=1, useMassSpring=1, springElasticStiffness=40, springDampingStiffness=40,
-                          springDampingAllDirections=1, useSelfCollision=0, frictionCoeff=10.0, useFaceContact=1,
-                          collisionMargin=0.001)
-    # return p.loadSoftBody("sphere_smooth.obj", basePosition=basePos, scale=0.5, mass=0.1, useNeoHookean=1,
-    #                       NeoHookeanMu=400, NeoHookeanLambda=600, NeoHookeanDamping=0.001, useSelfCollision=1,
-    #                       frictionCoeff=.5, collisionMargin=0.001)
-
-sphere = _load_softbody([0, 0, 1])
-
-p.changeDynamics(sphere, -1, mass=0.1)
+p.setAdditionalSearchPath("/home/mbed/Projects/rl-physnet/config")
 p.setRealTimeSimulation(1)
 p.setGravity(0, 0, -10)
 
-while (1):
-    restitution = p.readUserDebugParameter(restitutionId)
-    lidX = p.readUserDebugParameter(localInertiaDiagonalXId)
-    lidY = p.readUserDebugParameter(localInertiaDiagonalYId)
-    lidZ = p.readUserDebugParameter(localInertiaDiagonalZId)
+model = p.loadURDF("pisa_foot/pisa_foot.urdf", [0, 0, 1], [0, 0, 0, 1])
 
-    localInertiaDiagonal = [lidX, lidY, lidZ]
-    lateralFriction = p.readUserDebugParameter(lateralFrictionId)
-    spinningFriction = p.readUserDebugParameter(spinningFrictionId)
-    rollingFriction = p.readUserDebugParameter(rollingFrictionId)
-    p.changeDynamics(plane, -1, lateralFriction=lateralFriction)
-    p.changeDynamics(plane, -1, restitution=restitution)
-    p.changeDynamics(sphere, -1, lateralFriction=lateralFriction)
-    p.changeDynamics(sphere, -1, spinningFriction=spinningFriction)
-    p.changeDynamics(sphere, -1, rollingFriction=rollingFriction)
-    p.changeDynamics(sphere, -1, restitution=restitution)
-    p.changeDynamics(sphere, -1, localInertiaDiagonal=localInertiaDiagonal)
+while (1):
+    pass
+
+# restitutionId = p.addUserDebugParameter("restitution", 0, 1, 0.5)
+#
+# localInertiaDiagonalXId = p.addUserDebugParameter("localInertiaDiagonalX", 0, 3, 0.2)
+# localInertiaDiagonalYId = p.addUserDebugParameter("localInertiaDiagonalY", 0, 3, 0.2)
+# localInertiaDiagonalZId = p.addUserDebugParameter("localInertiaDiagonalZ", 0, 3, 0.2)
+#
+# lateralFrictionId = p.addUserDebugParameter("lateral friction", 0, 1, 0.5)
+# spinningFrictionId = p.addUserDebugParameter("spinning friction", 0, 1, 0.03)
+# rollingFrictionId = p.addUserDebugParameter("rolling friction", 0, 1, 0.03)
+#
+# plane = p.loadURDF("plane.urdf", [0, 0, 0])
+# aaa = p.loadURDF("cube.urdf", [0, 0, 3], flags=p.URDF_USE_SELF_COLLISION)
+# p.changeDynamics(aaa, -1, mass=0.1)
+#
+#
+# def _load_softbody(basePos):
+#     return p.loadSoftBody("cube.obj", basePosition=basePos, scale=0.5, mass=0.1, useNeoHookean=0,
+#                           useBendingSprings=1, useMassSpring=1, springElasticStiffness=40, springDampingStiffness=40,
+#                           springDampingAllDirections=1, useSelfCollision=0, frictionCoeff=10.0, useFaceContact=1,
+#                           collisionMargin=0.001)
+#     # return p.loadSoftBody("sphere_smooth.obj", basePosition=basePos, scale=0.5, mass=0.1, useNeoHookean=1,
+#     #                       NeoHookeanMu=400, NeoHookeanLambda=600, NeoHookeanDamping=0.001, useSelfCollision=1,
+#     #                       frictionCoeff=.5, collisionMargin=0.001)
+#
+# sphere = _load_softbody([0, 0, 1])
+#
+# p.changeDynamics(sphere, -1, mass=0.1)
+# p.setRealTimeSimulation(1)
+# p.setGravity(0, 0, -10)
+#
+# while (1):
+#     restitution = p.readUserDebugParameter(restitutionId)
+#     lidX = p.readUserDebugParameter(localInertiaDiagonalXId)
+#     lidY = p.readUserDebugParameter(localInertiaDiagonalYId)
+#     lidZ = p.readUserDebugParameter(localInertiaDiagonalZId)
+#
+#     localInertiaDiagonal = [lidX, lidY, lidZ]
+#     lateralFriction = p.readUserDebugParameter(lateralFrictionId)
+#     spinningFriction = p.readUserDebugParameter(spinningFrictionId)
+#     rollingFriction = p.readUserDebugParameter(rollingFrictionId)
+#     p.changeDynamics(plane, -1, lateralFriction=lateralFriction)
+#     p.changeDynamics(plane, -1, restitution=restitution)
+#     p.changeDynamics(sphere, -1, lateralFriction=lateralFriction)
+#     p.changeDynamics(sphere, -1, spinningFriction=spinningFriction)
+#     p.changeDynamics(sphere, -1, rollingFriction=rollingFriction)
+#     p.changeDynamics(sphere, -1, restitution=restitution)
+#     p.changeDynamics(sphere, -1, localInertiaDiagonal=localInertiaDiagonal)
--- scripts/train_haptic.py
+++ scripts/train_haptic.py
@@ -16,20 +16,20 @@ ENV_CONFIG = yaml.safe_load(open("../config/env.yaml", 'r'))
 def start(args):
     myenv = world.environment.pusher.PusherEnvGenerator(ENV_CONFIG)
 
+    # setup an optimization
+    os.makedirs(args.logs_path, exist_ok=True)
     eta = tf.Variable(args.lr)
     eta_value = tf.keras.optimizers.schedules.ExponentialDecay(args.lr, args.lr_decay_steps, args.lr_decay_rate)
     eta.assign(eta_value(0))
     optimizer = tf.keras.optimizers.Adam(eta)
     ckpt = tf.train.Checkpoint(optimizer=optimizer, model=myenv.haptic_regressor)
+    ckpt_man = tf.train.CheckpointManager(ckpt, args.logs_path, max_to_keep=10)
+    train_writer = tf.summary.create_file_writer(args.logs_path + "/train")
 
-    os.makedirs(args.logs_path, exist_ok=True)
-    # ckpt_man = tf.train.CheckpointManager(ckpt, args.logs_path, max_to_keep=10)
-    # train_writer = tf.summary.create_file_writer(args.logs_path + "/train")
-    # val_writer = tf.summary.create_file_writer(args.logs_path + "/val")
-    # test_writer = tf.summary.create_file_writer(args.logs_path + "/test")
-
-    for _ in range(NUM_EPISODES):
-        for _ in range(NUM_ACTIONS):
+    # start training
+    metric_loss = tf.keras.metrics.Mean(name="MeanLoss")
+    for n_ep in range(NUM_EPISODES):
+        for n_act in range(NUM_ACTIONS):
             action = world.action.primitives.PushAction.random_sample()
             observations, reward, done, info = myenv.step(action=action)
 
@@ -42,10 +42,20 @@ def start(args):
                 gradients = tape.gradient(loss, myenv.haptic_regressor.trainable_variables)
                 optimizer.apply_gradients(zip(gradients, myenv.haptic_regressor.trainable_variables))
 
-            print(loss)
+            metric_loss.update_state(loss.numpy())
 
+        # do each epoch
+        eta.assign(eta_value(0))
+        ckpt_man.save()
         myenv.reset()
         myenv.haptic_regressor.reset_states()
+
+        # add to the tensorboard
+        print("Episode: {}, mean loss: {}".format(n_ep, metric_loss.result().numpy()))
+        with train_writer.as_default():
+            tf.summary.scalar(metric_loss.name, metric_loss.result().numpy(), step=n_ep)
+            tf.summary.scalar(eta.name, eta.value().numpy(), step=n_ep)
+        train_writer.flush()
 
     myenv.stop_sim()
 
